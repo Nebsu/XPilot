ccb9569370cdd08acf09d08e9b75b6f10f20db69
DAI zhou <zhoudai12@gmail.com>
2022-02-07 00:10:34 +0100

    Upload New File

---
+++ Obstacle.java
@@ -0,0 +1,16 @@
+package Xpilot;
+import java.awt.*;
+
+public class Obstacle {
+    int []x;
+    int []y;
+    public Obstacle(int x[],int y[]){
+        this.x=x;
+        this.y=y;
+    }
+    public void draw(Graphics g){
+        g.setColor(Color.YELLOW);
+        Polygon polygon=new Polygon(x,y,Math.min(x.length,y.length));
+        g.fillPolygon(polygon);
+    }
+}
